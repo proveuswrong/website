1f2ec63a67afeab4da1ab510b0c90f429dfe670f
Ferit Tunçer <ferit@cryptolab.net>
2022-02-25 17:41:20 +0300

    fixes #2

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import * as styles from "./header.module.scss";
@@ -26,69 +29,64 @@ const handleMouseLeave = (e) => {
     document.getElementById("main").style.opacity = "100";
 };
 
-const isBrowser = typeof window !== "undefined";
+const Header = ({ siteTitle }) => {
+  const home = useRef(null);
+  const projects = useRef(null);
+  const about = useRef(null);
 
-const doesPathEqualsToCurrentPathname = (pathname) => {
-  console.debug(pathname);
-  if (!isBrowser) {
-    return false;
-  } else {
-    console.log(`actual: ${window.location.pathname}`);
-    console.log(window.location.pathname == pathname);
-    return window.location.pathname == pathname;
-  }
+  useEffect(() => {
+    window.location.pathname == "/" && home.current.classList.add("grayed-out");
+    window.location.pathname == "/projects/" &&
+      projects.current.classList.add("grayed-out");
+    window.location.pathname == "/about/" &&
+      about.current.classList.add("grayed-out");
+  }, []);
+
+  return (
+    <header>
+      <div className={styles.container}>
+        <Link to="/">
+          <h1
+            id="brand"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className={styles.h1}
+          >
+            PROVE <br /> US <br /> WRONG
+          </h1>
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li
+              ref={home}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <a href="/">HOME</a>
+            </li>
+
+            <li
+              ref={projects}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <a href="/projects">PROJECTS</a>
+            </li>
+
+            <li
+              ref={about}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              <a href="/about">ABOUT</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
-
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className={styles.container}>
-      <Link to="/">
-        <h1
-          id="brand"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          className={styles.h1}
-        >
-          PROVE <br /> US <br /> WRONG
-        </h1>
-      </Link>
-
-      <nav className={styles.nav}>
-        <ul>
-          <li
-            className={` ${
-              doesPathEqualsToCurrentPathname("/") ? "grayed-out" : ""
-            }`}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <a href="/">HOME</a>
-          </li>
-
-          <li
-            className={` ${
-              doesPathEqualsToCurrentPathname("/projects/") ? "grayed-out" : ""
-            }`}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <a href="/projects">PROJECTS</a>
-          </li>
-
-          <li
-            className={` ${
-              doesPathEqualsToCurrentPathname("/about/") ? "grayed-out" : ""
-            }`}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <a href="/about">ABOUT</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
