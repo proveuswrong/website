13e0afa50eda04242c5b808beee23d116e6d70ec
Ferit Tunçer <ferit@cryptolab.net>
2022-03-14 10:58:13 +0300

    open graph seo missing properties

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,13 +5,18 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
-function Seo({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
+// import logo from "../images/icon.png";
+
+const constructUrl = (baseUrl, path) =>
+  !baseUrl || !path ? null : `${baseUrl}${path}`;
+
+function Seo({ description, lang, meta, title, imageUrl, imageAlt }) {
+  const { query, site } = useStaticQuery(
     graphql`
       query {
         site {
@@ -19,14 +24,28 @@ function Seo({ description, lang, meta, title }) {
             title
             description
             author
+            siteUrl
+          }
+        }
+        ogImageDefault: file(relativePath: { eq: "icon.png" }) {
+          childImageSharp {
+            fixed(height: 260, width: 260) {
+              src
+            }
           }
         }
       }
     `
-  )
+  );
 
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+  const defaultImageUrl = constructUrl(
+    site.siteMetadata.siteUrl,
+    query.ogImageDefault?.childImageSharp?.fixed?.src
+  );
+  const ogImageUrl = imageUrl || defaultImageUrl;
+
+  const metaDescription = description || site.siteMetadata.description;
+  const defaultTitle = site.siteMetadata?.title;
 
   return (
     <Helmet
@@ -68,22 +87,34 @@ function Seo({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `og:image`,
+          content: ogImageUrl,
+        },
+        {
+          name: `twitter:card`,
+          content: imageUrl ? `summary_large_image` : `summary`,
+        },
+        {
+          name: `twitter:image:alt`,
+          content: imageAlt || defaultTitle,
+        },
       ].concat(meta)}
     />
-  )
+  );
 }
 
 Seo.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
+};
 
 Seo.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
-}
+};
 
-export default Seo
+export default Seo;
