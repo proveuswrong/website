81aebdf05026b2243fbc5725ce4e29b53f9ec718
Ferit Tunçer <ferit@cryptolab.net>
2022-03-14 13:34:58 +0300

    manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Prove Us Wrong`,
+        short_name: `PUW`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: "#171717",
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: "#171717",
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
